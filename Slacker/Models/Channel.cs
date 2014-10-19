3b02ed3328166cddd4a3735a69c422d4e1c8e4f7
Wii, Kuo <wiikuo.tw@gmail.com>
2014-10-19 22:26:33 +0800

    add notify icon and can scan unread count.

---
+++ Slacker/Models/Channel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slacker.Models
+{
+	class Channel
+	{
+		public string ID
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public int UnreadCount
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ Slacker/Models/Team.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slacker.Models
+{
+	/// <summary>
+	/// Slack Team
+	/// </summary>
+	class Team
+	{
+		/// <summary>
+		/// Gets or sets the token.
+		/// </summary>
+		public string Token
+		{
+			get;
+			set;
+		}
+
+		public List<Channel> Channels
+		{
+			get;
+			private set;
+		}
+
+		public Team()
+		{
+			this.Channels = new List<Channel>();
+		}
+	}
+}
